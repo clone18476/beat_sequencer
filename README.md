151101029a8f56034857e248a5ede15b54f0d720
clone18476 <cameroncousins18@gmail.com>
2021-06-23 16:33:32 -0500

    3rd commit, fully functional, needs front-end

---
+++ README.md
@@ -0,0 +1,7 @@
+This Beats Sequencer is a 16-step drum sequencer
+
+Open up the published link, start writing in notes!
+
+https://clone18476.github.io/beat_sequencer/
+
+Further functionality is currently in development.
--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+
 function sequencer(){
     const claps = new Tone.Player('https://clone18476.github.io/beat_sequencer/SFX/clap-analog.wav').toDestination();
     const snare = new Tone.Player('https://clone18476.github.io/beat_sequencer/SFX/snare-pinch.wav').toDestination();
+    const kick = new Tone.Player('https://clone18476.github.io/beat_sequencer/SFX/tom-808.wav').toDestination();
     let index = 0;
-    
-    Tone.Transport.scheduleRepeat(repeat,'8n')
+
+
+    Tone.Transport.scheduleRepeat(repeat,'16n')
     Tone.Transport.start();
 
     function repeat(){
         let step =  index % 8;
         let selectedSfx = document.querySelector(`.top  input:nth-child(${step + 1})`);
         let selectedSfx2 = document.querySelector(`.bottom  input:nth-child(${step + 1})`);
+        let selectedSfx3 = document.querySelector(`.middle  input:nth-child(${step + 1})`);
         if (selectedSfx.checked){
             claps.start();  
         }
         if (selectedSfx2.checked){
             snare.start();  
+        }        
+        if (selectedSfx3.checked){
+            kick.start();  
         }
         index ++
     }
@@ -22,3 +29,4 @@ function sequencer(){
 
 
 sequencer();
+
